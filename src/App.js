601da81bb6c912c10a5c7193a8ea141541d972c5
yajiv88606 <86217541+yajiv88606@users.noreply.github.com>
2021-11-05 13:29:11 +0530

    made some responsive changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Header from "./components/header";
+
 import Home from "./components/home";
 import "./css/App.scss";
 function App() {
   return (
     <div>
-      <Header />
       <Home />
     </div>
   );
--- src/components/home.js
+++ src/components/home.js
@@ -46,41 +46,46 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <InputGroup
-          className="mb-3 search-bar"
-          value={this.state.mealName}
-          name="mealName"
-          onChange={this.changeValue}
-        >
-          <FormControl
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
-            aria-describedby="basic-addon2"
-          />
-          <Button
-            type="submit"
-            variant="outline-secondary"
-            className="btn btn-secondary"
-            id="button-addon2"
-            onClick={this.searchMeals}
+        <div className="headers">
+          <h1>Meals App</h1>
+          <InputGroup
+            className="search-bar"
+            value={this.state.mealName}
+            name="mealName"
+            onChange={this.changeValue}
           >
-            Search
-          </Button>
-          <Button
-            type="submit"
-            variant="outline-secondary"
-            className="btn btn-secondary"
-            id="button-addon2"
-            onClick={this.getRandomMeal}
-          >
-            Suprise Me!
-          </Button>
-        </InputGroup>
-        {this.state.listOfMeals.length > 0
-          ? this.state.listOfMeals.map((e) => (
-              <Meal meal={e} style={{ margin: "5px 0px" }} />
-            ))
-          : null}
+            <FormControl
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <Button
+              type="submit"
+              variant="outline-secondary"
+              className="btn btn-secondary"
+              id="button-addon2"
+              onClick={this.searchMeals}
+            >
+              Search
+            </Button>
+            <Button
+              type="submit"
+              variant="outline-secondary"
+              className="btn btn-secondary"
+              id="button-addon2"
+              onClick={this.getRandomMeal}
+            >
+              Suprise Me!
+            </Button>
+          </InputGroup>
+        </div>
+        <div className="content">
+          {this.state.listOfMeals.length > 0
+            ? this.state.listOfMeals.map((e) => (
+                <Meal meal={e} style={{ margin: "5px 0px" }} />
+              ))
+            : null}
+        </div>
       </div>
     );
   }
